2b36b1d91e594d87d187713e4946619843e24b35
Nguyen Huu Tien <tien.nguyenhuu10@mobifone.vn>
2024-12-19 16:23:08 +0700

    add https

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import envConfig from "@/config";
 
 export default function Home() {
   console.log("check3", envConfig);
+  const isBrowser = typeof window !== "undefined";
+  console.log(isBrowser);
 
-  return <Button className="bg-background">Click me</Button>;
+  return <Button>Click me</Button>;
 }
--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,13 @@ import { z } from "zod";
 const configSchema = z.object({
   NEXT_PUBLIC_API_ENDPOINT: z.string(),
   NEXT_PUBLIC_URL: z.string(),
-  DB_USER: z.string(),
+  // DB_USER: z.string(),
 });
 
 const configProject = configSchema.safeParse({
   NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
-  DB_USER: process.env.DB_USER,
+  // DB_USER: process.env.DB_USER,
 });
 
 if (!configProject.success) {
@@ -19,3 +19,7 @@ if (!configProject.success) {
 
 const envConfig = configProject.data;
 export default envConfig;
+export type Locale = (typeof locales)[number];
+
+export const locales = ["en", "vi"] as const;
+export const defaultLocale: Locale = "vi";
